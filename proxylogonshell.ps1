7797a88f5f39e5e19e4e2a2a52fe51876c3c57f7
Deepak Sharma <supersonic08@gmail.com>
2023-04-17 12:04:34 -0400

    Update proxylogonshell.ps1

--- proxylogonshell.ps1
+++ proxylogonshell.ps1
@@ -1,8 +1,9 @@
 Get-WinEvent -LogName "MSExchange Management" | 
 Where-Object {
-    ($_.Message -like "*new-mailboxexportrequest*" -and $_.Message -like "*aspx*") -or 
-    ($_.Message -like "*new-exchangecertificate*" -and $_.Message -like "*aspx*") -or 
-    ($_.Message -like "*set-oabvirtualdirectory*" -and $_.Message -like "*script*")
+  ($_.Message -like "*new-mailboxexportrequest*" -and $_.Message -like "*aspx*") -or 
+  ($_.Message -like "*new-exchangecertificate*" -and $_.Message -like "*aspx*") -or 
+  ($_.Message -like "*set-oabvirtualdirectory*" -and $_.Message -like "*script*")
 } |
 Select-Object TimeCreated, Message |
+Format-List -Force |
 Out-File "C:\proxylogonshell.txt"
